c263f5870f928ed2b47075e3c9ff8234e5d4e5c3
marmelad444 <1111marmelad1111@gmail.com>
2025-03-06 09:08:41 +0000

    lab3 сделаны контроллеры

--- note-app/routes/web.php
+++ note-app/routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\GoodbyeController;
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\TodoController;
 
 Route::get('/', [WelcomeController::class, 'welcome'])->name('welcome');
 
 Route::get('/goodbye', [GoodbyeController::class, 'goodbye'])->name('goodbye');
+
+Route::resource('note', NoteController::class);
